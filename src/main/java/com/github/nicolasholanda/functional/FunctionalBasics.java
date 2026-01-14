c68d8f5648a9004a083939f6bee048556cb9c40f
nicolasholanda <nikolas.nhm14@gmail.com>
2026-01-14 09:08:26 -0300

    Added examples for predicate, function, consumer, supplier

---
+++ src/main/java/com/github/nicolasholanda/functional/FunctionalBasics.java
@@ -0,0 +1,34 @@
+package com.github.nicolasholanda.functional;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class FunctionalBasics {
+    public static void execute() {
+        System.out.println("- Predicate test one condition:");
+        Predicate<Integer> isEven = n -> n % 2 == 0;
+        System.out.println("Is 4 even? " + isEven.test(4));
+        System.out.println("Is 7 even? " + isEven.test(7));
+
+
+        System.out.println("- Function transform input to output:");
+        Function<String, Integer> stringLength = s -> s.length();
+        System.out.println("Length of 'Hello': " + stringLength.apply("Hello"));
+
+
+
+        System.out.println("- Consumer consume input without return:");
+        Consumer<String> printer = s -> System.out.println("Message: " + s);
+        printer.accept("Hello World");
+
+
+
+        System.out.println("- Supplier supply a value:");
+        Supplier<Double> randomSupplier = () -> Math.random();
+        System.out.println("Random value: " + randomSupplier.get());
+
+    }
+}
+
--- src/main/java/com/github/nicolasholanda/functional/FunctionalDemo.java
+++ src/main/java/com/github/nicolasholanda/functional/FunctionalDemo.java
@@ -1,4 +1,8 @@
 package com.github.nicolasholanda.functional;
 
 public class FunctionalDemo {
+    static void main() {
+        System.out.println("--- Built-in functional interfaces ---");
+        FunctionalBasics.execute();
+    }
 }
