db56d63cb38521cc06a352ca21b393df735271ac
nicolasholanda <nikolas.nhm14@gmail.com>
2026-01-15 20:38:08 -0300

    Added constructor and instance methods reference

--- src/main/java/com/github/nicolasholanda/functional/FunctionalMethodRef.java
+++ src/main/java/com/github/nicolasholanda/functional/FunctionalMethodRef.java
@@ -1,12 +1,19 @@
 package com.github.nicolasholanda.functional;
 
 import java.util.List;
+import java.util.function.Function;
 
 class Person {
 
     String name;
 
+    Person(String name) {
+        this.name = name;
+    }
+
     static boolean isLongName(String name) { return name.length() > 5; }
+
+    String toUpperCase() { return name.toUpperCase(); }
 }
 
 public class FunctionalMethodRef {
@@ -15,6 +22,15 @@ public class FunctionalMethodRef {
 
         System.out.println("Static method reference:");
         names.stream().filter(Person::isLongName).forEach(System.out::println);
+
+        System.out.println("Instance method reference:");
+        Person person = new Person("john");
+        Function<Person, String> upperFunc = Person::toUpperCase;
+        System.out.println(upperFunc.apply(person));
+
+        System.out.println("Constructor reference:");
+        Function<String, Person> personFactory = Person::new;
+        System.out.println(personFactory.apply("factory").name);
     }
 }
 
