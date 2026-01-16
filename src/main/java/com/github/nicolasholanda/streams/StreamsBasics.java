774c88bf01434f8392fc3937f6663a255180b2f5
nicolasholanda <nikolas.nhm14@gmail.com>
2026-01-16 08:17:45 -0300

    Added demo for map, reduce and collect

--- src/main/java/com/github/nicolasholanda/streams/StreamsBasics.java
+++ src/main/java/com/github/nicolasholanda/streams/StreamsBasics.java
@@ -10,6 +10,28 @@ public class StreamsBasics {
         numbers.stream()
             .filter(n -> n % 2 == 0)
             .forEach(System.out::println);
+
+
+
+        System.out.println("Map to square:");
+        numbers.stream()
+            .map(n -> n * n)
+            .limit(5)
+            .forEach(System.out::println);
+
+
+
+        System.out.println("Reduce sum:");
+        int sum = numbers.stream().reduce(0, Integer::sum);
+        System.out.println(sum);
+
+
+
+        System.out.println("\nCollect to list:");
+        List<Integer> evens = numbers.stream()
+            .filter(n -> n % 2 == 0)
+            .toList();
+        System.out.println(evens);
     }
 }
 
