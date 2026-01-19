94afb9fe9469ab6a45593b67a50d9b4c0ed35694
nicolasholanda <nikolas.nhm14@gmail.com>
2026-01-19 09:51:27 -0300

    Added fixed thread pool executor demo

---
+++ src/main/java/com/github/nicolasholanda/concurrency/ConcurrencyDemo.java
@@ -0,0 +1,11 @@
+package com.github.nicolasholanda.concurrency;
+
+public class ConcurrencyDemo {
+
+    static void main() throws Exception {
+
+        System.out.println("--- Executor basics ---");
+        ExecutorBasics.execute();
+    }
+}
+
---
+++ src/main/java/com/github/nicolasholanda/concurrency/ExecutorBasics.java
@@ -0,0 +1,25 @@
+package com.github.nicolasholanda.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorBasics {
+
+    static void execute() {
+
+        System.out.println("Fixed thread pool:");
+        try(ExecutorService fixed = Executors.newFixedThreadPool(2)) {
+            for (int i = 1; i <= 4; i++) {
+                int taskId = i;
+                fixed.submit(() -> {
+                    System.out.println("Task " + taskId + " on " + Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException _) {
+                    }
+                });
+            }
+        }
+    }
+}
+
