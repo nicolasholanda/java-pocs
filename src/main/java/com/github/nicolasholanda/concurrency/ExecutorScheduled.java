d061a994b6d62a08f9a932912885859061c2fc6b
nicolasholanda <nikolas.nhm14@gmail.com>
2026-01-20 18:54:36 -0300

    Added example for scheduleAtFixedRate

--- src/main/java/com/github/nicolasholanda/concurrency/ExecutorScheduled.java
+++ src/main/java/com/github/nicolasholanda/concurrency/ExecutorScheduled.java
@@ -13,6 +13,20 @@ public class ExecutorScheduled {
             scheduler.schedule(() -> System.out.println("Task executed after 1s"), 1, TimeUnit.SECONDS);
             Thread.sleep(2000);
         }
+
+
+
+        System.out.println("scheduleAtFixedRate:");
+        try (ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1)) {
+            scheduler.scheduleAtFixedRate(
+                () -> System.out.println("Fixed rate task in " + System.currentTimeMillis()),
+                0,
+                1,
+                TimeUnit.SECONDS
+            );
+            Thread.sleep(3500);
+        }
+
     }
 }
 
