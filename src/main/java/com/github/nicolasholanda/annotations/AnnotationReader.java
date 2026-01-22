4353ec784c4c787b52093cf49f817c042d058a80
nicolasholanda <nikolas.nhm14@gmail.com>
2026-01-22 09:35:29 -0300

    Reading method annotations

--- src/main/java/com/github/nicolasholanda/annotations/AnnotationReader.java
+++ src/main/java/com/github/nicolasholanda/annotations/AnnotationReader.java
@@ -1,6 +1,7 @@
 package com.github.nicolasholanda.annotations;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class AnnotationReader {
 
@@ -22,6 +23,18 @@ public class AnnotationReader {
             if (field.isAnnotationPresent(Column.class)) {
                 Column column = field.getAnnotation(Column.class);
                 System.out.println("Field: " + field.getName() + ", Column: " + column.name() + ", Nullable: " + column.nullable());
+            }
+        }
+
+        System.out.println("Reading method annotations:");
+        for (Method method : userClass.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(Execute.class)) {
+                Execute execute = method.getAnnotation(Execute.class);
+                System.out.println("Method: " + method.getName() + ", Priority: " + execute.priority());
+            }
+            if (method.isAnnotationPresent(Deprecated.class)) {
+                Deprecated deprecated = method.getAnnotation(Deprecated.class);
+                System.out.println("Method " + method.getName() + " deprecated - " + deprecated.reason());
             }
         }
     }
