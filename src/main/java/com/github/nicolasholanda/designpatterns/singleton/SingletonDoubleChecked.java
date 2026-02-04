c3088b35ef910090944b685137f8799664e19508
nicolasholanda <nikolas.nhm14@gmail.com>
2026-02-04 09:44:46 -0300

    Added double checked singleton

---
+++ src/main/java/com/github/nicolasholanda/designpatterns/singleton/SingletonDoubleChecked.java
@@ -0,0 +1,21 @@
+package com.github.nicolasholanda.designpatterns.singleton;
+
+public class SingletonDoubleChecked {
+
+    private static volatile SingletonDoubleChecked instance;
+
+    private SingletonDoubleChecked() {
+    }
+
+    public static SingletonDoubleChecked getInstance() {
+        if (instance == null) {
+            synchronized (SingletonDoubleChecked.class) {
+                if (instance == null) {
+                    instance = new SingletonDoubleChecked();
+                }
+            }
+        }
+        return instance;
+    }
+}
+
