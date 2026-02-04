be0d0d24c599636c9ed2bcf3ab67154d4ded9254
nicolasholanda <nikolas.nhm14@gmail.com>
2026-02-04 09:27:18 -0300

    Added Singleton

---
+++ src/main/java/com/github/nicolasholanda/designpatterns/singleton/Singleton.java
@@ -0,0 +1,17 @@
+package com.github.nicolasholanda.designpatterns.singleton;
+
+public class Singleton {
+
+    private static Singleton instance;
+
+    private Singleton() {
+        // private constructor to prevent instantiation
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
